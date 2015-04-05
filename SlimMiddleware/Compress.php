8938f22489d784aa58d3ced1120281357207a754
Aaron Saray <me@aaronsaray.com>
2015-04-05 16:10:22 -0500

    updated directory location

---
+++ SlimMiddleware/Compress.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Slim Middleware Compressor
+ *
+ * @author Aaron Saray
+ */
+
+namespace SlimMiddleware;
+
+/**
+ * Class Compress
+ * @package SlimMiddleware
+ */
+class Compress
+{
+
+}
