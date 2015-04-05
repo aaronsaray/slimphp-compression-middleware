3952c4b3b8e60e037a5794f2dcba5f25f89e49ad
Aaron Saray <me@aaronsaray.com>
2015-04-05 16:50:56 -0500

    added the handler

--- src/Compress.php
+++ src/Compress.php
@@ -3,6 +3,7 @@
  * Slim Middleware Compressor
  *
  * @author Aaron Saray
+ * @version 0.1
  */
 
 namespace AaronSaray\SlimPHPMiddleware;
@@ -15,9 +16,13 @@ class Compress extends \Slim\Middleware
 {
     /**
      * The middleware calling function
+     *
+     * Please note: there are no real functions to this in this first version
      */
     public function call()
     {
+        ob_start('ob_gzhandler');
+
         $this->next->call();
     }
 }
